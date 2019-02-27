b862dbda960b978f4131ae04daf7ac24f196ed28
gillianbc <gszemeti@gmail.com>
2019-02-27 20:00:18 +0000

    Used post 1.7 syntax and updated the build config in the pom

--- src/main/java/gillian/bladenclark/Application.java
+++ src/main/java/gillian/bladenclark/Application.java
@@ -1,7 +1,20 @@
 package gillian.bladenclark;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Application {
     
+    public void greet(){
+        //Generics only came into java at 1.7
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hello");
+        greetings.add("Goodbye");
+
+        for (String word : greetings) {
+            System.out.println("Greeting " + word);
+        }
+    }
+    // Constructor
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -9,6 +22,7 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+        Application app = new Application();
+        app.greet();
     }
 }
